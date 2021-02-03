4e56a4217b5103a59ef7f8e7466d8156019d743d
hendrasolih <hendra.solih.jp@gmail.com>
2021-02-03 22:14:28 +0700

    notif fix name

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
   //   console.log(msg)
   //   io.to(id_recipient).to(id).emit("chat message", msg);
   // });
-  socket.on("transfer", (amount, sender, recipient) => {
+  socket.on("transfer", (amount, sender, sender_name, recipient) => {
     console.log("sender = " + sender);
     console.log("recipient = " + recipient);
     console.log("amount = " + amount);
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
     );
     io.to(recipient).emit(
       "transfer in",
-      `${sender} mengirim dana sebesar ${amount}`,
+      `${sender_name} mengirim dana sebesar ${amount}`,
       amount
     );
     //----
--- src/helpers/middlewares/checkToken.js
+++ src/helpers/middlewares/checkToken.js
@@ -1,36 +1,37 @@
-const jsonwebtoken = require('jsonwebtoken')
-const db = require('../../config/mySQL')
-const form = require('../form')
-
+const jsonwebtoken = require("jsonwebtoken");
+const db = require("../../config/mySQL");
+const form = require("../form");
 
 module.exports = {
   isRegistered: (req, res, next) => {
-    const { email } = req.body
+    const { email } = req.body;
     const checkAvailable = new Promise((resolve, reject) => {
-      const queryStr = `SELECT email FROM users WHERE email = ?`
+      const queryStr = `SELECT email FROM users WHERE email = ?`;
       db.query(queryStr, email, (err, data) => {
         if (!err) {
           if (!data[0]) {
             resolve({
-              msg: `success`
-            })
+              msg: `success`,
+            });
           } else {
             reject({
-              msg: `email telah digunakan!`
-            })
+              msg: `email telah digunakan!`,
+            });
           }
         } else {
           reject({
             msg: `SQLquery ERROR!`,
-            details: err
-          })
+            details: err,
+          });
         }
-      })
-    }).then((result) => {
-      next()
-    }).catch((error) => {
-      res.status(500).json(error)
+      });
     })
+      .then((result) => {
+        next();
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
   },
   isLogin: (req, res, next) => {
     const bearerToken = req.header("x-access-token");
@@ -38,134 +39,141 @@ module.exports = {
     if (!bearerToken) {
       res.json({
         msg: `Please login first`,
-        status: 401 //unauthorized access
-      })
+        status: 401, //unauthorized access
+      });
     } else {
       // Bearer Token
       // [Bearer, Token]
-      const token = bearerToken.split(" ")[1]
+      const token = bearerToken.split(" ")[1];
       const checkBlacklist = new Promise((resolve, reject) => {
-        const queryStr = `SELECT token FROM blocklist_token WHERE token = ?`
+        const queryStr = `SELECT token FROM blocklist_token WHERE token = ?`;
         db.query(queryStr, token, (err, data) => {
           if (!err) {
             if (!data[0]) {
-              console.log('a')
-              resolve(token)
+              console.log("a");
+              resolve(token);
             } else {
-              console.log('b')
+              console.log("b");
               reject({
-                msg: `Invalid token => ${data[0].token}. Already logout`
-              })
+                msg: `Invalid token => ${data[0].token}. Already logout`,
+              });
             }
           } else {
-            console.log('c')
+            console.log("c");
             reject({
-              msg: `check Token ERROR!`
-            })
+              msg: `check Token ERROR!`,
+            });
+          }
+        });
+      })
+        .then((result) => {
+          //cek decodedToken apakah cocok
+          try {
+            console.log(result);
+            const decodedToken = jsonwebtoken.verify(
+              result,
+              process.env.SECRET_KEY
+            );
+            //asign decodedToken to req
+            req.decodedToken = decodedToken;
+            next(); //meneruskan ke proses selanjutnya
+          } catch (err) {
+            res.json({
+              msg: `Token invalid, wrong SECRET_KEY`,
+            });
           }
         })
-      }).then((result) => {
-        //cek decodedToken apakah cocok
-        try {
-          console.log(result)
-          const decodedToken = jsonwebtoken.verify(result, process.env.SECRET_KEY)
-          //asign decodedToken to req
-          req.decodedToken = decodedToken
-          next() //meneruskan ke proses selanjutnya
-        } catch (err) {
-          res.json({
-            msg: `Token invalid, wrong SECRET_KEY`
-          })
-        }
-      }).catch((error) => {
-        res.json(error)
-      })
+        .catch((error) => {
+          res.json(error);
+        });
     }
   },
   isSeller: (req, res, next) => {
-    const { level } = req.decodedToken
+    const { level } = req.decodedToken;
     if (level != 2) {
-      form.error(res,
-        {
-          status: 401,
-          msg: `Unauthorized Access`,
-          details: `Yout dont have permission to access this page.`
-        }
-      )
+      form.error(res, {
+        status: 401,
+        msg: `Unauthorized Access`,
+        details: `Yout dont have permission to access this page.`,
+      });
     } else {
-      next()
+      next();
     }
   },
   phoneUsed: (req, res, next) => {
-    const { phone } = req.body
-    console.log(req.body)
+    const { phone } = req.body;
+    console.log(req.body);
     if (phone != undefined) {
       const checkAvailable = new Promise((resolve, reject) => {
-        const queryStr = `SELECT phone FROM users WHERE phone = ?`
+        const queryStr = `SELECT phone FROM users WHERE phone = ?`;
         db.query(queryStr, phone, (err, data) => {
           if (!err) {
             if (data.length > 0) {
               reject({
                 status: 401,
-                message: `No. HP sudah digunakan`
-              })
+                message: `No. HP sudah digunakan`,
+              });
             } else {
               resolve({
-                status: 200
-              })
+                status: 200,
+              });
             }
           } else {
             reject({
               status: 500,
               message: `INTERNAL SERVER ERROR`,
-              details: err
-            })
+              details: err,
+            });
           }
-        })
-      }).then((result) => {
-        next()
-      }).catch((error) => {
-        res.status(error.status).json(error)
+        });
       })
+        .then((result) => {
+          next();
+        })
+        .catch((error) => {
+          res.status(error.status).json(error);
+        });
     } else {
-      next()
+      next();
     }
   },
   isPIN: (req, res, next) => {
-    const email = req.decodedToken.email
-    const PIN = req.header("x-access-PIN")
+    const email = req.decodedToken.email;
+    const PIN = req.header("x-access-PIN");
     if (!PIN) {
       res.status(401).json({
         status: 401,
-        message: `invalid PIN`
-      })
+        message: `invalid PIN`,
+      });
     } else {
       const checkPIN = new Promise((resolve, reject) => {
-        const queryStr = `SELECT * FROM users WHERE email = ? AND pin = ?`
+        const queryStr = `SELECT * FROM users WHERE email = ? AND pin = ?`;
         db.query(queryStr, [email, PIN], (err, data) => {
           if (!err) {
             if (data.length > 0) {
               resolve({
-                status: 200
-              })
+                status: 200,
+              });
             } else {
               reject({
                 status: 404,
-                message: `PIN tidak teridentifikasi`
-              })
+                message: `PIN tidak teridentifikasi`,
+              });
             }
           } else {
             reject({
               status: 500,
-              message: err
-            })
+              message: err,
+            });
           }
-        })
-      }).then((result) => {
-        next()
-      }).catch((error) => {
-        res.status(error.status).json(error)
+        });
       })
+        .then((result) => {
+          next();
+        })
+        .catch((error) => {
+          res.status(error.status).json(error);
+        });
     }
-  }
-}
+  },
+};
